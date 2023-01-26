1f736c7752ba24e24813aed21c713414261db45c
Christoph Kormesser <christoph.kormesser@gmail.com>
2023-01-26 18:02:55 +0100

    Initial commit

---
+++ src/main/java/com/example/sqare/SqareApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sqare/SqareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
